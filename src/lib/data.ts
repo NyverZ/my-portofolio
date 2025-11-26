6f33723d4c4841a020de56137482cbeab1db21a2
Your Name <you@example.com>
2025-11-26 14:27:01 +0800

    First commit

---
+++ src/lib/data.ts
@@ -0,0 +1,160 @@
+export const personalInfo = {
+  name: "Muh Noval Thurfah",
+  location: "Kota Bima, Indonesia",
+  email: "nyverzthurfah@gmail.com",
+  github: "https://github.com/NyverZ",
+  linkedin: "https://www.linkedin.com/in/thurfah-nyverz-663a18373/",
+  profilePicture: "/portofolio.jpeg",
+  heroDescription:
+    "Saya adalah seorang Web Developer yang memiliki keahlian dalam membangun website dan aplikasi web yang fungsional, responsif, dan berorientasi pada pengalaman pengguna. Dengan pengalaman menggunakan teknologi modern seperti React, Next.js, TailwindCSS, dan REST API, saya mampu merancang tampilan antarmuka yang menarik sekaligus memastikan performa yang optimal.",
+};
+
+export const workExperience = [
+  {
+    company: "Lorem Ipsum Corp",
+    location: "Lorem City",
+    position: "Lorem Engineer",
+    period: "Jan 2020 - Present",
+    achievements: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+      "Excepteur sint occaecat cupidatat non proident.",
+      "Sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.",
+    ],
+  },
+  {
+    company: "Dolor Sit Amet",
+    location: "Ipsumville",
+    position: "Software Developer",
+    period: "Feb 2019 - Jan 2020",
+    achievements: [
+      "Morbi in sem quis dui placerat ornare.",
+      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam.",
+    ],
+  },
+  {
+    company: "Consectetur Inc.",
+    location: "Adipiscing, Ipsum",
+    position: "Intern",
+    period: "Jun 2018 - Feb 2019",
+    achievements: [
+      "Praesent dapibus, neque id cursus faucibus.",
+      "Fusce feugiat malesuada odio.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Lorem Ipsum University",
+    location: "Lorem City",
+    degree: "BSc Lorem Ipsum",
+    period: "2015 - 2019",
+    achievements: [
+      "President of Lorem Ipsum Club",
+      "Organized multiple lorem events",
+      "Represented university in national competitions.",
+      "Graduated with honors.",
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: ["JavaScript", "TypeScript", "Go", "PHP", "Python"],
+  frontendDevelopment: [
+    "React",
+    "Next.js",
+    "TypeScript",
+    "TailwindCSS",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["NodeJS", "ExpressJS", "Laravel"],
+  databaseAndStorage: ["MySQL", "PostgreSQL", "MongoDB"],
+  cloudAndDevOps: ["AWS", "Docker", "Kubernetes"],
+  toolsAndServices: [
+    "Git",
+    "Jenkins",
+    "Terraform",
+    "SitValidator",
+    "Laragon",
+    "AmetMonitor",
+    "ConsecteturPanel",
+    "AdipiscingTrigger",
+  ],
+};
+
+export const projects = [
+  {
+    title: "VerzAnimeList",
+    github: "https://github.com/NyverZ/anime-list",
+    description: [
+      "Verzanimelist adalah kumpulan judul-judul anime yang disusun untuk berbagai tujuan, seperti memberikan rekomendasi, menampilkan yang paling populer, atau merencanakan tontonan. Daftar ini biasanya memuat informasi seperti judul anime, peringkat, jumlah episode, rating, jumlah penggemar, serta deskripsi singkat. Dengan daftar ini, penggemar dapat dengan mudah melacak anime yang telah ditonton, sedang ditonton, atau yang ingin ditonton. Verzanimelist menjadi alat yang berguna untuk mengatur pengalaman menonton dan menemukan anime baru yang sesuai dengan preferensi."
+    ],
+  },
+  {
+    title: "Dolor Sit Amet App",
+    github: "https://github.com/loremipsum/dolorapp",
+    description: [
+      "Morbi in sem quis dui placerat ornare.",
+      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam.",
+      "Praesent dapibus, neque id cursus faucibus.",
+      "Fusce feugiat malesuada odio.",
+      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.",
+    ],
+  },
+];
+
+export const awards = [
+  {
+    name: "Lorem Ipsum Award",
+    issuer: "Lorem Organization",
+    date: "Jan 2020",
+    type: "International",
+    position: "First Place",
+  },
+  {
+    name: "Dolor Sit Amet Prize",
+    issuer: "Ipsum Foundation",
+    date: "Feb 2021",
+    type: "National",
+    position: "Runner-up",
+  },
+  {
+    name: "Consectetur Hackathon",
+    issuer: "Adipiscing Org",
+    date: "Mar 2022",
+    type: "National",
+    position: "Winner",
+  },
+  {
+    name: "Vestibulum Event",
+    issuer: "Vestibulum College",
+    date: "Apr 2022",
+    type: "National",
+    position: "First Prize",
+  },
+  {
+    name: "Curabitur Hackfest",
+    issuer: "Curabitur Institute",
+    date: "May 2022",
+    type: "National",
+    position: "Second Prize",
+  },
+  {
+    name: "Praesent Hacks",
+    issuer: "Praesent Group",
+    date: "Jun 2022",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Fusce Hack",
+    issuer: "Fusce Club",
+    date: "Jul 2022",
+    type: "National",
+    position: "Most Impactful Hack",
+  },
+];
